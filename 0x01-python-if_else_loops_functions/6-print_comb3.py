0b520f16f45837ec30b8e9ae25262c8fe51f32ac
MaryEnny <eniolamary38@gmail.com>
2024-03-24 04:03:07 -0700

    python

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -2,6 +2,5 @@
 for a in range(0, 9):
     for b in range(0, 10):
         if a < b:
-            print("{}{}, ".format(a, b),end='')
-            print("89")
-
+            print("{}{}, ".format(a, b), end='')
+print("89")
