628c0866ca1a3a0f263596cf25f44020f1f5fe38
MaryEnny <eniolamary38@gmail.com>
2024-04-05 13:32:26 -0700

    python

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 for a in range(ord('z'), ord('a') -1, -1):
     if a % 2 == 0:
-        print(chr(a), end='')
+        diff = 0
     else:
-        print(chr(a - 32), end='')
+        diff = 32
+        print('{}'.format(chr(i - diff)), end='')
