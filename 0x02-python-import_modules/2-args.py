ba1700cdc52313798d8f578b7a7aad5e83727646
MaryEnny <eniolamary38@gmail.com>
2024-04-10 00:26:51 -0700

    python

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -5,7 +5,7 @@ if __name__ == "__main__":
     if a == 0:
         print("0 arguments.")
     elif a == 1:
-         print("1 argument:")
+        print("1 argument:")
     else:
         print("{} arguments:".format(a))
     for i in range(a):
