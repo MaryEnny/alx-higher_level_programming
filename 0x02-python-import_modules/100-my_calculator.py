82b6982b8f6956546f00d025e44535e3be129261
MaryEnny <eniolamary38@gmail.com>
2024-04-10 02:05:47 -0700

    pyt

---
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    import calculator_1
+    import sys
+    c = len(sys.argv) - 1
+    if c != 3:
+        print("Usage: ./100-my_calculator.py <a> <operator> <b>")
+        sys.exit(1)
+    ops = {"*", "-", "/", "+"}
+    if sys.argv[2] not in ops:
+        print("Unknown operator. Available operators: +, -, * and /")
+        sys.exit(1)
+    a = int(sys.argv[1])
+    b = int(sys.argv[3])
+    if sys.argv[2] == "-":
+        print("{} - {} = {}".format(a, b, (a - b)))
+    elif sys.argv[2] == "+":
+        print("{} + {} = {}".format(a, b, (a + b)))
+    elif sys.argv[2] == "/":
+        print("{} / {} = {}".format(a, b, (a / b)))
+    else:
+        print("{} * {} = {}".format(a, b, (a * b)))
